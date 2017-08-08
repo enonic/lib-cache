e54fb8bb71106613bbe35c9460cf71f3e708ad4b
Alex Rodriguez <aro@enonic.com>
2017-08-08 11:53:23 +0200

    Implement remove-function in cache.js (#1)

--- src/main/java/com/enonic/lib/cache/CacheBean.java
+++ src/main/java/com/enonic/lib/cache/CacheBean.java
@@ -1,14 +1,16 @@
 package com.enonic.lib.cache;
 
 import java.util.concurrent.Callable;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import com.google.common.cache.Cache;
 
 public final class CacheBean
 {
-    private final Cache<Object, Object> cache;
+    private final Cache<String, Object> cache;
 
-    public CacheBean( final Cache<Object, Object> cache )
+    public CacheBean( final Cache<String, Object> cache )
     {
         this.cache = cache;
     }
@@ -28,4 +30,19 @@ public final class CacheBean
     {
         return (int) this.cache.size();
     }
+
+    public void remove( final String key )
+    {
+        cache.invalidate( key );
+    }
+
+    public void removePattern( final String keyRegex )
+    {
+        final Pattern pattern = Pattern.compile( keyRegex );
+        final Stream<String> keyStream = cache.asMap().keySet().stream().filter( ( k ) -> pattern.matcher( k ).matches() );
+
+        final Iterable<String> keys = keyStream::iterator;
+        cache.invalidateAll( keys );
+    }
+
 }
--- src/test/resources/test/cache-test.js
+++ src/test/resources/test/cache-test.js
@@ -43,3 +43,62 @@ exports.testCache = function () {
 exports.testCompatibility = function () {
     assert.assertTrue(cacheLib === require('/lib/xp/cache'));
 };
+
+exports.testRemove = function () {
+
+    var cache = cacheLib.newCache({
+        size: 100,
+        expire: 10
+    });
+
+    assert.assertEquals(0, cache.getSize());
+
+    var numCalled = 0;
+    var calcFunction = function () {
+        numCalled++;
+
+        return {
+            num: numCalled,
+            name: 'value' + numCalled
+        };
+    };
+
+    var result = cache.get('key1', calcFunction);
+    assert.assertEquals(1, result.num);
+    assert.assertEquals('value1', result.name);
+    assert.assertEquals(1, cache.getSize());
+
+    cache.remove('key1');
+    assert.assertEquals(0, cache.getSize());
+};
+
+exports.testRemovePattern = function () {
+
+    var cache = cacheLib.newCache({
+        size: 100,
+        expire: 10
+    });
+
+    assert.assertEquals(0, cache.getSize());
+
+    var numCalled = 0;
+    var calcFunction = function () {
+        numCalled++;
+
+        return {
+            num: numCalled,
+            name: 'value' + numCalled
+        };
+    };
+
+    cache.get('key1', calcFunction);
+    cache.get('key2', calcFunction);
+    cache.get('key3', calcFunction);
+    cache.get('k1', calcFunction);
+    cache.get('k2', calcFunction);
+
+    assert.assertEquals(5, cache.getSize());
+
+    cache.removePattern('key.*');
+    assert.assertEquals(2, cache.getSize());
+};
